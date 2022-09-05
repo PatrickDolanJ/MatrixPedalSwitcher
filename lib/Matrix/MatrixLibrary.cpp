acc573877e619f92c72f312bb63028c396ab832d
Mama_Dutchy <datpoland@gmail.com>
2022-09-05 01:29:51 -0400

    tis library-ized. At least internally. Not entirely sure if you can use libraries outside of PlatformIOs registry.? Will figure that out.

--- lib/Matrix/MatrixLibrary.cpp
+++ lib/Matrix/MatrixLibrary.cpp
@@ -1,61 +1,63 @@
 // things to add. hardcoded ummmm bits for each x and y address. this would allow you to pass human readable numbers, x and y numbers
 // currently you have to construct the byte 
 // https://www.farnell.com/datasheets/1793938.pdf
-#include <Wire.h>
-#include <matrix_library.h>
 #include <Arduino.h>
+#include <Wire.h>
+#include <MatrixLibrary.h>
 
-//const byte AGD2188_ADDRESS = 0x71;  //Arduino uses 7 bit addresses. this probably means that arduino 
+
+const byte AGD2188_ADDRESS = 0x71;  //Arduino uses 7 bit addresses. this probably means that arduino 
 //only has 128 possible adress that it can write to. you probably have to drop the least signifigant bit 
 //which we guess means we drop the highest bit meaning the one all the way to the left. If you have 
 //an 8 bit address, drop the LSB and shift right
-//const byte NOW = 0x01;
+const byte NOW = 0x01;
 // 0x71 remember that 0x just neans its a hex number and the two following digits are the actual hex 71 = 1110001
 // in the datasheet figure 33, this matches the A0-A2 address bytes
 
 // Lay out the constants for reading back the switch states
-// const byte X0 = 0x34;
-// const byte X1 = 0x3C;
-// const byte X2 = 0x74;
-// const byte X3 = 0x7C;
-// const byte X4 = 0x35;
-// const byte X5 = 0x3D;
-// const byte X6 = 0x75;
-// const byte X7 = 0x7D;
+const byte X0 = 0x34;
+const byte X1 = 0x3C;
+const byte X2 = 0x74;
+const byte X3 = 0x7C;
+const byte X4 = 0x35;
+const byte X5 = 0x3D;
+const byte X6 = 0x75;
+const byte X7 = 0x7D;
 //Making the first bit to tell to turn off or on connection
-// const byte ON_BIT = 0b1;
-// const byte OFF_BIT = 0b0;
+const byte ON_BIT = 0b1;
+const byte OFF_BIT = 0b0;
 //////////////////////////
 
 // //Settign up x array for converting to BYTE
-// const byte W_X0 = 0b0000;
-// const byte W_X1 = 0b0001;
-// const byte W_X2 = 0b0010;
-// const byte W_X3 = 0b0011;
-// const byte W_X4 = 0b0100;
-// const byte W_X5 = 0b0101;
-// const byte W_X6 = 0b1000;
-// const byte W_X7 = 0b0001;
+const byte W_X0 = 0b0000;
+const byte W_X1 = 0b0001;
+const byte W_X2 = 0b0010;
+const byte W_X3 = 0b0011;
+const byte W_X4 = 0b0100;
+const byte W_X5 = 0b0101;
+const byte W_X6 = 0b1000;
+const byte W_X7 = 0b0001;
 
-//const byte x_byte_array[8] = {W_X0,W_X1,W_X2,W_X3,W_X4,W_X5,W_X6,W_X7};
+const byte x_byte_array[8] = {W_X0,W_X1,W_X2,W_X3,W_X4,W_X5,W_X6,W_X7};
 /////////////////
 
 // //Setting up y adress for converting to BYTE
-// const byte W_Y0 = 0b000;
-// const byte W_Y1 = 0b001;
-// const byte W_Y2 = 0b010;
-// const byte W_Y3 = 0b011;
-// const byte W_Y4 = 0b100;
-// const byte W_Y5 = 0b101;
-// const byte W_Y6 = 0b110;
-// const byte W_Y7 = 0b111;
+const byte W_Y0 = 0b000;
+const byte W_Y1 = 0b001;
+const byte W_Y2 = 0b010;
+const byte W_Y3 = 0b011;
+const byte W_Y4 = 0b100;
+const byte W_Y5 = 0b101;
+const byte W_Y6 = 0b110;
+const byte W_Y7 = 0b111;
 
-//const byte y_byte_array[8]= {W_Y0,W_Y1,W_Y2,W_Y3,W_Y4,W_Y5,W_Y6,W_Y7};
+const byte y_byte_array[8]= {W_Y0,W_Y1,W_Y2,W_Y3,W_Y4,W_Y5,W_Y6,W_Y7};
 ///////////////////
 AGD2188::AGD2188()
 {
   Wire.begin(); // join i2c bus (address optional for master)
   //Serial.begin(115200);
+  //Serial.println("THIS IS FROM INSIDE THE MATRIX NEYO");
 }
 
 //void loop()
@@ -172,5 +174,6 @@ void AGD2188::wipe_chip()
       delay(100);
     }
   }
+  Serial.println("Chip wiped");
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include <Arduino.h>
-#include <matrix_library.h>
+#include "MatrixLibrary.h"
 
+
+AGD2188 matrix;
+bool wipe_chip_once = true;
 
 //#include <Wire.h>
 void setup() {
   Serial.begin(9600);
+  matrix = AGD2188();
 }
 
 void loop() {
-  Serial.println("Farts");
+  if(wipe_chip_once)
+  {
+  matrix.wipe_chip();
+  wipe_chip_once = false;
+  }
 }
